8a617e8224918edf69cc0d0625cba3c6d7fe483c
Aledgit <edalvarez@uc.cl>
2024-12-18 02:59:49 -0300

    Simplified ReadTheDocs Documentation

--- docs/conf.py
+++ docs/conf.py
@@ -1,30 +1,20 @@
 import os
 import sys
 
-# Recursively add all subdirectories in 'src' to the Python path
-src_path = os.path.abspath("../src")
-for root, dirs, _ in os.walk(src_path):
-    sys.path.insert(0, root)
+# Add source directory to the Python path
+sys.path.insert(0, os.path.abspath("../src"))
 
 # -- Project information -----------------------------------------------------
 project = "fair-sciml"
-copyright = "2024, Eduardo Alvarez, Paul Escapil, Adolfo Parra"
+copyright = "2024"
 author = "Eduardo Alvarez, Paul Escapil, Adolfo Parra"
 release = "1.0"
 
 # -- General configuration ---------------------------------------------------
-extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.napoleon",
-    "sphinx.ext.autosummary",
-    "sphinx.ext.viewcode",
-    "sphinx_autodoc_typehints",
-]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon"]
 
-autosummary_generate = True  # Automatically generate summary tables
-autodoc_default_options = {
-    "members": True,
-    "undoc-members": True,
-    "private-members": False,
-    "show-inheritance": True,
-}
+# Specify the master doc (entry point of your documentation)
+master_doc = "index"
+
+# Options for HTML output
+html_theme = "alabaster"  # Use a lightweight theme
